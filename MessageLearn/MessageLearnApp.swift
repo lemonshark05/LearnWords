c7d12f91fc5f6eb84a52dd2b6349067525ded14e
zhimin meng <mengzhimin5@gmail.com>
2023-08-08 13:49:44 -0700

    Add files via upload

---
+++ MessageLearn/MessageLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageLearnApp.swift
+//  MessageLearn
+//
+//  Created by lemonshark on 2023/8/8.
+//
+
+import SwiftUI
+
+@main
+struct MessageLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
